47c8921ee81088b8cbcfb6bf576a7e3324dbb429
Simon Cropp <simon.cropp@gmail.com>
2022-03-03 09:02:04 +1100

    .

---
+++ Sample/SqlServerTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+
+public class SqlServerTests
+{
+    static SqlInstance sqlInstance = new(
+            "SqlServerTests",
+            connection =>
+            {
+                var sqlConnection = (SqlConnection) connection;
+                var server = new Server(new ServerConnection(sqlConnection));
+                server.ConnectionContext.ExecuteNonQuery(@"
+CREATE TABLE
+MyTable(Value int);
+GO
+");
+                return Task.CompletedTask;
+            });
+
+    [Fact]
+    public async Task Run()
+    {
+        using var database = await sqlInstance.Build();
+        DiagnosticListener.AllListeners.Subscribe(new Subscriber());
+        using var command = database.Connection.CreateCommand();
+        command.CommandText = "select Value from MyTable";
+        command.ExecuteScalar();
+        Assert.True(Subscriber.SqlOnNextHit);
+    }
+}
+
+public class Subscriber : IObserver<DiagnosticListener>
+{
+    public void OnCompleted() { }
+
+    public void OnError(Exception error) { }
+
+    public void OnNext(DiagnosticListener listener)
+    {
+        if (listener.Name == "SqlClientDiagnosticListener")
+        {
+            SqlOnNextHit = true;
+        }
+    }
+
+    public static bool SqlOnNextHit;
+}
---
+++ Sample/Usings.cs
@@ -0,0 +1,7 @@
+﻿global using System;
+global using System.Threading.Tasks;
+global using Xunit;
+global using LocalDb;
+global using Microsoft.Data.SqlClient;
+global using Microsoft.SqlServer.Management.Common;
+global using Microsoft.SqlServer.Management.Smo;
